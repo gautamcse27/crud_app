30f515ca3b1ea55884a45c43a87ebf04564edc92
gautamcse27 <khushi27gkcse@gmail.com>
2025-02-10 21:03:52 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,72 +2,56 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import RealAbout from './RealAbout';
 import Contact from './Contact';
 import UserApp from './UserApp';
 import UserProfile from './UserProfile';
 import SearchUser from './SearchUser'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Navbar, Nav, Container, Card } from "react-bootstrap";
+import './App.css';
 const App = () => {
     return (
+        <div className="d-flex flex-column min-vh-100">
         <Router>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container">
-                    <Link to="/" className="navbar-brand">
-                        My App
-                    </Link>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarNav"
-                        aria-controls="navbarNav"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <Link to="/" className="nav-link">
-                                    Home
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/users" className="nav-link">
-                                    Users
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/searchuser" className="nav-link">
-                                    Search a User 
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/about" className="nav-link">
-                                    Report
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/contact" className="nav-link">
-                                    Contact
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
+                <Navbar.Brand href="/">Employee Management</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/users" style={{ color: "#FFFFFF" }}>Manage Users</Nav.Link>
+              <Nav.Link as={Link} to="/searchuser" style={{ color: "#FFFFFF" }}>Search a User</Nav.Link>
+              <Nav.Link as={Link} to="/about" style={{ color: "#FFFFFF" }}>Report</Nav.Link>
+              <Nav.Link as={Link} to="/contact" style={{ color: "#FFFFFF" }}>Contact Us</Nav.Link>
+              <Nav.Link as={Link} to="/realabout" style={{ color: "#FFFFFF" }}>About Us</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+                    
                 </div>
             </nav>
-
+            <div className="flex-grow-1">
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/users" element={<UserApp />} />
                 <Route path="/searchuser" element={<SearchUser/>} />
                 <Route path="/about" element={<About />} />
+                <Route path="/realabout" element={<RealAbout />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/users/:id" element={<UserProfile />} />
             </Routes>
+            </div>
+            <footer className="bg-dark text-white text-center py-3 mt-4">
+                    <Container>
+                        <p>&copy; {new Date().getFullYear()} Employee Management App. All rights reserved.</p>
+                        <Nav className="justify-content-center">
+                            <Nav.Link as={Link} to="/realabout" style={{ color: "#FFFFFF" }}>About</Nav.Link>
+                            <Nav.Link as={Link} to="/contact" style={{ color: "#FFFFFF" }}>Contact</Nav.Link>
+                        </Nav>
+                    </Container>
+                </footer>
         </Router>
+        </div>
     );
 };
 
