6bb3f61bbecc48a55dea423cfbfc748977fae07c
gautamcse27 <khushi27gkcse@gmail.com>
2025-02-10 21:05:34 +0530

    Create RealAbout.js

---
+++ frontend/src/RealAbout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RealAbout = () => {
+    return (
+        <div className="container mt-5">
+            <h1 className="text-center">About Us</h1>
+            <p className="text-center">This is the About page.</p>
+        </div>
+    );
+};
+
+export default RealAbout;
